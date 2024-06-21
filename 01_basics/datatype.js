0bc8a49294ae398050b2db76af1a41c5dfdce727
DevanandEkka <ekkadevanand@gmail.com>
2024-06-21 09:36:35 +0000

    datatype done

---
+++ 01_basics/datatype.js
@@ -0,0 +1,22 @@
+"use strict";//treat all JS code as newer version
+//alert(3+3) we are using nodeJS,not browser
+console.log(5+111)
+console.log(`Deva`)
+
+
+
+let name="Devanand"
+let age = 25
+let isLoggedIn=false/true
+
+//Preemtive
+//number=> 2 to the power 53
+//bigint
+//string=>""
+//boolean=>true/false
+//null=> Standalone value
+//undefined=>
+//symbol=>Unique (react)
+console.log(typeof undefined)//undefined
+console.log(typeof null)//object
+
