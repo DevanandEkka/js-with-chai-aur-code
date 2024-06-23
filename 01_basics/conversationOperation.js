0ad4c093693d529af5c17781ea728a533d1934e7
DevanandEkka <ekkadevanand@gmail.com>
2024-06-23 09:52:06 +0000

    done

---
+++ 01_basics/conversationOperation.js
@@ -0,0 +1,24 @@
+//let score =null
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log( valueInNumber)
+
+//"33" => 33 (string)
+//"33abc" => NaN (number)
+//true=>1; false=>0
+
+let isLoggedIn =""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+// 1 =>true; 0 => false
+//" " => false
+//"Deva" => true
+let someNumber = 40
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log();
