e808034512af271bcb876973e44cea73e04cfded
DevanandEkka <ekkadevanand@gmail.com>
2024-06-24 07:38:52 +0000

    dataType Summary

---
+++ 01_basics/datatype_Summary.js
@@ -0,0 +1,36 @@
+// Primitive
+
+
+// 7 type: String, Number, boolean, null ,undefined,symbol,BigInt
+
+
+const score = 100
+const scoreValue = 100.43
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+
+const id = Symbol(`141`)
+const AnotherId = Symbol(`141`)
+console.log( id===AnotherId)
+const BigInt = 14145445575555544n
+
+
+
+// Refrence( Non primtive)
+//Array, Objects,Functions
+
+const Heroes =["Shaktimaan" , "Naagraj" ," Doga"]
+console.log(Heroes)
+
+
+let Myobj = {
+    Name:"Devanand", //written inside the curly bracelet {}
+    agg:24,
+}
+
+const myFunction = function(){
+    console.log("Hello World")
+}
