efac9992b738c44f5259a24c2f95b2da86bf02a3
DevanandEkka <ekkadevanand@gmail.com>
2024-06-24 07:39:57 +0000

    comparison

---
+++ 01_basics/comparison.js
@@ -0,0 +1,25 @@
+//  console.log(2 > 1)
+//  console.log(2 >= 1) 
+//  console.log(2 < 1)
+//  console.log(2 == 1)
+//  console.log(2 !=1)
+
+// console.log("2" > 1)
+// console.log(2 < "1") This comparison method is invalid don't use this 
+
+
+// console.log(null>0)
+// console.log(null>=0)
+// console.log(null<0)
+// console.log(null==0)
+
+
+
+// console.log(undefined >0)
+// console.log(undefined>=0)
+// console.log( undefined==0)
+// console.log( null>=undefined)
+// console.log(undefined>=null)
+// console.log("4"===1)
+
+// ****Avoid this kind of coparision****//
