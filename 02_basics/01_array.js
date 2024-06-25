1b6d5a9c67b60e07c064ec9d907dbdf93c0d7722
DevanandEkka <ekkadevanand@gmail.com>
2024-06-25 11:06:53 +0000

    Array and iys methods

---
+++ 02_basics/01_array.js
@@ -0,0 +1,42 @@
+// array
+const myArr = [0, 1, 2, 3, 4, 5]
+// console.log(myArr[0])
+//Array methods
+// myArr.push(6)
+// console.log(myArr);
+// myArr.push(7)
+// console.log(myArr);
+// myArr.pop()
+// console.log(myArr)
+
+// myArr.unshift(11)
+// console.log(myArr)
+
+
+// myArr.shift()
+// console.log(myArr)
+// myArr.shift()
+// // console.log(myArr)
+// console.log(myArr)
+
+// console.log(myArr.includes(2))
+// console.log(myArr.includes(10))
+
+// console.log(myArr.indexOf(2))
+// console.log(myArr.indexOf(10))
+// const newArr = myArr.join()
+// console.log(myArr)
+// console.log( newArr)
+
+//slice,splice
+
+console.log("A",myArr);
+const myn1 = myArr.slice(1,3)
+console.log(myn1)
+console.log("B" , myArr)
+
+const myn2 = myArr.splice(1,3)
+console.log(myn2);
+
+console.log("C" , myArr);
+
