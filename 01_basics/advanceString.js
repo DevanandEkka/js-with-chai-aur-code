5368431a2691f569318b659ab63cfb5e8ab4d1d3
DevanandEkka <ekkadevanand@gmail.com>
2024-06-25 06:48:33 +0000

    learn latest printing techniqe

---
+++ 01_basics/advanceString.js
@@ -0,0 +1,22 @@
+const name =` Devanand Ekka`
+let age = 24
+
+//avoid this method to print console.log (  name +  age + " value")
+
+ console.log(`My name =${name} , my age =${age}`)//always prefer this method to print with back-tick(` `)
+
+ const gameName =  String("Gumla-JH");
+ console.log(gameName[4]);
+ console.log(gameName.__proto__);
+
+ console.log(gameName.length);
+ console.log(gameName.toLocaleUpperCase());
+ console.log(gameName.charAt(1));
+ console.log(gameName.indexOf('a'))
+ const url = "https://Devanand.com/devanand%20ekka"
+ console.log(url.replace('%20','-------'))//variable name . replace ( 'identify whatever to rplace'  , 'write that value whatever you want to place')
+ 
+ console.log(url.includes('devanand'))
+ console.log(url.includes('sundar'))
+ console.log(gameName.split('--','3'))
+ 
