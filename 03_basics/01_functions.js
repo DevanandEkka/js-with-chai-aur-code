a69fcab03b80861d077f2b3b6803989f95ce7c26
DevanandEkka <ekkadevanand@gmail.com>
2024-07-01 11:07:23 +0000

    some extra

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,64 +1,64 @@
-function sayMyName(){
-    console.log("D");
-    console.log("E");
-    console.log("V");
-    console.log("A");
-    console.log("N");
-    console.log("A");
-    console.log("N");
-    console.log("D");
-}
+// function sayMyName(){
+//     console.log("D");
+//     console.log("E");
+//     console.log("V");
+//     console.log("A");
+//     console.log("N");
+//     console.log("A");
+//     console.log("N");
+//     console.log("D");
+// }
 
 //  sayMyName()    // Here,sayMyName is refrence & () execution
 
-// function addTwoNumbers(number1 ,number2) // when you pass function defination is called parameters 
-// {
-//     console.log(number1 + number2)
-// }
-// addTwoNumbers(8,8)// when you pass values are   called Arguments 
+// // function addTwoNumbers(number1 ,number2) // when you pass function defination is called parameters 
+// // {
+// //     console.log(number1 + number2)
+// // }
+// // addTwoNumbers(8,8)// when you pass values are   called Arguments 
 
-function addTwoNumbers(number1 ,number2) {
-    // let result = number1 + number2
-    return number1 + number2//after return NO cade will execute
+// function addTwoNumbers(number1 ,number2) {
+//     // let result = number1 + number2
+//     return number1 + number2//after return NO cade will execute
   
-}
+// }
 
-let result = addTwoNumbers(11,11)
-// console.log("Result:", result);
+// let result = addTwoNumbers(11,11)
+// // console.log("Result:", result);
 
-function loginUserMessage(username){
-    if (username===undefined) {
-        // console.log("Please Enter Your Username");
-        return
-    }
-    return `${username} just logged in`
-}
+// function loginUserMessage(username){
+//     if (username===undefined) {
+//         // console.log("Please Enter Your Username");
+//         return
+//     }
+//     return `${username} just logged in`
+// }
 
-// console.log(loginUserMessage("Devanand"))
-// console.log(loginUserMessage())
+// // console.log(loginUserMessage("Devanand"))
+// // console.log(loginUserMessage())
 
 function calculateCartPrice(...num1){   //rest operator
     return num1
 }
-// console.log(calculateCartPrice(200,400,800,400,))
+ console.log(calculateCartPrice(200,400,800,400,))
 
-//how to pass object in function
+// //how to pass object in function
 
-const user ={
-    username: "Devanand",
-    price: 1999
-}
+// const user ={
+//     username: "Devanand",
+//     price: 1999
+// }
 
-function handleObject(anyobject){
-    // console.log(`user name is ${anyobject.username}  price is ${anyobject.price}`)
-}
-// handleObject(user)
-handleObject(
-    {
-        username:"Sam",
-        price:199
-    }
-)
+// function handleObject(anyobject){
+//     // console.log(`user name is ${anyobject.username}  price is ${anyobject.price}`)
+// }
+// // handleObject(user)
+// handleObject(
+//     {
+//         username:"Sam",
+//         price:199
+//     }
+// )
 
 const myNewArr = [ 200, 400, 100, 600]
 
@@ -66,5 +66,7 @@ function returnSecondValue(getArray){
     return getArray[1]
 
 }
-// console.log(returnSecondValue(myNewArr));
+console.log(returnSecondValue(myNewArr));
 console.log(returnSecondValue([200,400,100,600]));
+
+
