6845efa98352ea061c2e87425c0578b6a6f8ac0f
DevanandEkka <ekkadevanand@gmail.com>
2024-07-03 10:32:46 +0000

    for in loop with object and array

---
+++ 05_iteration/four.js
@@ -0,0 +1,16 @@
+const myObject = {
+    JS: "javaScript",
+    cpp: "C++",
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in myObject) {
+    // console.log(`${key} Extentions of = ${myObject[key]}`)
+}
+
+const programming =[ "js", "py", "cpp", "java"]
+
+for (const key in programming) {
+    console.log(`${programming[key]}`)
+}
